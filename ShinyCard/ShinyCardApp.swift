0dbfbbfc2e1a40837e4341c6be43f8fee08088b8
Lawrence Reynolds <reynoldslawrence21@yahoo.co.uk>
2026-04-30 11:56:44 +0100

    Initial Commit

---
+++ ShinyCard/ShinyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShinyCardApp.swift
+//  ShinyCard
+//
+//  Created by Lawrence Reynolds on 30/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShinyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
